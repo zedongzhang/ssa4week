bc6068a834fad8f60999bc3efe3106b3ee93356f
zedongzhang <zedongzhang@yahoo.com>
2016-08-02 11:02:10 -0400

    second day commit

--- day1/example.js
+++ day1/example.js
@@ -1,3 +1,28 @@
+function domPrompt(msg) {
+	var myDivItem = document.createElement("div");
+	myDivItem.innerHTML = msg;
+	myDivItem.onclick = function(evt) {
+
+	};
+	document.body.appendChild(myDivItem);
+}
+
+function domAlert(msg) {
+	var myDivItem = document.createElement("div");
+	myDivItem.innerHTML = msg;
+	myDivItem.setAttribute('class', 'alert_class');
+	document.body.appendChild(myDivItem);
+}
+
+domPrompt("domPrompt domPrompt domPrompt domPrompt");
+
+var promptInput = document.getElementById("promptInput");
+promptInput.onkeydown = function(evt) {
+	if(evt.key == 'Enter') {
+		alert(promptInput.value);
+	}
+}
+
 var myrps = Math.floor(Math.random() * 3);
 var yourps = prompt("Give me your Rock (0), Paper (1) or Scissors (2)");
 var result;
